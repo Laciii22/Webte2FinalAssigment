7b406f606120588abab361297d795c4ac1ba17e3
Laciii22 <gardian.laci@gmail.com>
2024-05-12 16:46:37 +0200

    Word cloud

--- resources/js/question-result-scripts.js
+++ resources/js/question-result-scripts.js
@@ -1,8 +1,9 @@
+/* GRAPHS */
 document.addEventListener('DOMContentLoaded', function () {
     // Select all canvas elements with an id that includes 'pie-chart'
     const canvases = document.querySelectorAll('canvas[id^="pie-chart"]');
 
-    canvases.forEach(function(canvas) {
+    canvases.forEach(function (canvas) {
         const labels = JSON.parse(canvas.getAttribute('data-labels'));
         const data = JSON.parse(canvas.getAttribute('data-data'));
 
@@ -48,4 +49,132 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+
+
+    /* WORD CLOUD */
+
+    var wordCloudElements = document.querySelectorAll('[id^=word-cloud]');
+
+    wordCloudElements.forEach(function (wordCloudElement) {
+        var responses = JSON.parse(wordCloudElement.dataset.wordCloud);
+        var canvas = document.createElement('canvas');
+        canvas.width = wordCloudElement.offsetWidth;
+        canvas.height = wordCloudElement.offsetHeight; // Opraveno z parentHeight
+        console.log(canvas.height);
+
+        canvas.style.borderRadius = '10px'; // zaoblení rohů na 10px
+
+        canvas.style.border = '1px solid rgb(75,85,99)';
+
+
+        wordCloudElement.appendChild(canvas);
+
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'rgb(55,65,81)'; // Například tmavě šedá barva
+        // Vykreslení plněný obdélníku s touto barvou
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        var maxFontSize = 50;
+        var minFontSize = 8;
+        var textMargin = 5;
+        var maxCount = Math.max.apply(Math, responses.map(function (response) { return response.count; }));
+
+        function checkCollision(rect, rectangles) {
+            for (var i = 0; i < rectangles.length; i++) {
+                if (rect !== rectangles[i] &&
+                    rect.x < rectangles[i].x + rectangles[i].width &&
+                    rect.x + rect.width > rectangles[i].x &&
+                    rect.y < rectangles[i].y + rectangles[i].height &&
+                    rect.y + rect.height > rectangles[i].y) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        var placedRectangles = [];
+
+        responses.forEach(function (response) {
+            var fontSize = Math.max((response.count / maxCount) * maxFontSize, minFontSize);
+            ctx.font = fontSize + 'px Arial';
+            //console.log(fontSize);
+
+            // Generovanie náhodnej farby textu vo formáte hexadecimálneho kódu
+            var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            ctx.fillStyle = randomColor; // Nastaviť farbu textu na náhodnú farbu
+
+            var textWidth = ctx.measureText(response.value).width;
+            var x, y;
+            var collisionDetected = true;
+            while (collisionDetected) {
+                x = Math.random() * (canvas.width - textWidth);
+                y = Math.random() * (canvas.height - fontSize - textMargin) + fontSize;
+                var rect = { x: x, y: y - fontSize, width: textWidth, height: fontSize };
+                collisionDetected = checkCollision(rect, placedRectangles);
+            }
+
+            ctx.fillText(response.value, x, y);
+            placedRectangles.push({ x: x, y: y - fontSize, width: textWidth, height: fontSize });
+        });
+    });
+
+
+    var toggleButtons = document.querySelectorAll('[id^=toggleButton-]');
+
+    toggleButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var version = button.id.split('-')[1];
+            var wordCloud = document.getElementById('word-cloud-' + version);
+            var responseList = document.getElementById('response-list-' + version);
+    
+            if (!wordCloud.classList.contains('hidden')) {
+                wordCloud.classList.add('hidden');
+                responseList.classList.remove('hidden');
+            } else {
+                wordCloud.classList.remove('hidden');
+                responseList.classList.add('hidden');
+            }
+        });
+    });
+    
+
+
+
+
+
+    /*     PREKRYVAJU SA ALE NEPADAJU  */
+
+    /*  document.addEventListener('DOMContentLoaded', function() {
+            var wordCloudElements = document.querySelectorAll('[id^=word-cloud]');
+    
+            wordCloudElements.forEach(function(wordCloudElement) {
+                var responses = JSON.parse(wordCloudElement.dataset.wordCloud);
+                var canvas = document.createElement('canvas');
+                canvas.width = wordCloudElement.offsetWidth; // Nastaviť šírku plátna na šírku nadradeného prvku
+                canvas.height = 200;
+                wordCloudElement.appendChild(canvas);
+    
+                var ctx = canvas.getContext('2d');
+                var maxFontSize = 70;
+                var minFontSize = 10;
+                var textMargin = 5; // Miera okolo textu na zabránenie prekrytia
+                var maxCount = Math.max.apply(Math, responses.map(function(response) { return response.count; }));
+    
+                responses.forEach(function(response) {
+                    var fontSize = Math.max((response.count / maxCount) * maxFontSize, minFontSize);
+                    ctx.font = fontSize + 'px Arial';
+    
+                    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                    ctx.fillStyle = randomColor; // Nastaviť farbu textu na náhodnú farbu
+    
+                    var x = Math.random() * (canvas.width - ctx.measureText(response.value).width);
+                    var y = Math.random() * (canvas.height - fontSize - textMargin) + fontSize;
+    
+                    ctx.fillText(response.value, x, y);
+                });
+            });
+    
+        }); */
+
+
 });
--- resources/views/questions/question-result.blade.php
+++ resources/views/questions/question-result.blade.php
@@ -6,41 +6,67 @@
 
     <div class="min-h-screen">
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col md:flex-row gap-4 text-center">
-                <div class="pt-6 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg md:w-1/2">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="text-xl font-bold">Code: {{ $question->code }}</h2>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col md:flex-row gap-4 ">
+                <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg md:w-1/2">
+                    <div class="p-6">
+                        <label for="lesson" class="block text-sm font-medium text-gray-900 dark:text-white">Code:</label>
+                        <input name="lesson" type="text" value="{{ $question->code }} " class="lesson-input mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" value="{{$question->lesson}}" disabled />
                     </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        Title: {{ $question->title }}
+                    <div class="p-6">
+                        <label for="lesson" class="block text-sm font-medium text-gray-900 dark:text-white">Title:</label>
+                        <input name="lesson" type="text" value="{{ $question->title }} " class="lesson-input mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" value="{{$question->lesson}}" disabled />
                     </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        Lesson: {{ $question->lesson }}
+                    <div class="p-6">
+                        <label for="lesson" class="block text-sm font-medium text-gray-900 dark:text-white">Lesson:</label>
+                        <input name="lesson" type="text" value="{{ $question->lesson }} " class="lesson-input mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" value="{{$question->lesson}}" disabled />
                     </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        Active: {{ $question->active }}
+                    <div class="p-6">
+                        <label for="lesson" class="block text-sm font-medium text-gray-900 dark:text-white">Active:</label>
+                        <input name="lesson" type="text" value="{{ $question->active }} " class="lesson-input mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" value="{{$question->lesson}}" disabled />
                     </div>
                 </div>
+
+
+
                 @if($question->category != 'text')
                 @foreach($allResponses as $version => $responses)
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg md:w-1/2">
-                        <div class="p-6 text-white flex justify-center">
-                            <canvas id="pie-chart-{{ $version }}" style="max-width: 300px; max-height: 300px;" data-labels="{!! htmlspecialchars(json_encode($responses->pluck('value')), ENT_QUOTES, 'UTF-8') !!}" data-data="{!! htmlspecialchars(json_encode($responses->pluck('count')), ENT_QUOTES, 'UTF-8') !!}" data-version="{{ $version }}"></canvas>
-                        </div>
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg md:w-1/2">
+                    <div class="p-6 text-white flex justify-center">
+                        <canvas id="pie-chart-{{ $version }}" style="max-width: 300px; max-height: 300px;" data-labels="{!! htmlspecialchars(json_encode($responses->pluck('value')), ENT_QUOTES, 'UTF-8') !!}" data-data="{!! htmlspecialchars(json_encode($responses->pluck('count')), ENT_QUOTES, 'UTF-8') !!}" data-version="{{ $version }}"></canvas>
                     </div>
+                </div>
                 @endforeach
                 @else
+
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg md:w-1/2">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="text-xl font-bold">Responses:</h2>
+                        <div class="flex items-center mb-3">
+                            <h2 class="text-xl font-bold mr-5">Responses:</h2>
+                            @foreach($allResponses as $version => $responses)
+                            <label class="inline-flex items-center cursor-pointer">
+                                <input type="checkbox" id="toggleButton-{{ $version }}" class="sr-only peer" checked>
+                                <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
+                                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Word cloud</span>
+                            </label>
+                            @endforeach
+                        </div>
                         @foreach($allResponses as $version => $responses)
                         <div>Version {{ $version }}:</div>
-                        @foreach($responses as $response)
-                            <div>{{ $response->value }}</div>
-                        @endforeach
+                        <div id="word-cloud-{{ $version }}" class="min-h-80 mt-3" data-word-cloud="{{ json_encode($responses) }}"></div>
+                        <div id="response-list-{{ $version }}" class="hidden">
+                            @foreach($responses as $response)
+                            <div class="p-6">
+                                <input name="responses" type="text" value="{{ $response->count }}x {{ $response->value }}" class="lesson-input mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" disabled />
+                            </div>
+
+                            @endforeach
+                        </div>
                         @endforeach
                     </div>
                 </div>
+
+
+
                 @endif
             </div>
         </div>
